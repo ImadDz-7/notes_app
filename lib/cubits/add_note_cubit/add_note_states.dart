f8da19f8fd6dc9cd5eef24e7409fb5b176d1bff0
ImadDz-7 <imadbouamrirene97@gmail.com>
2024-03-06 15:51:06 +0100

    add add note states

---
+++ lib/cubits/add_note_cubit/add_note_states.dart
@@ -0,0 +1,12 @@
+abstract class AddNoteState {}
+
+class AddNoteInitialState extends AddNoteState {}
+
+class AddNoteLoadingState extends AddNoteState {}
+
+class AddNoteSuccessState extends AddNoteState {}
+
+class AddNoteFailureState extends AddNoteState {
+  final String errMessage;
+  AddNoteFailureState(this.errMessage);
+}
